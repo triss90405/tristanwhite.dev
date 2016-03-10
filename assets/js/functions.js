0e26dcac1958e20a097ceb6715a2e2c12cc94b35
Tristan White <oliver.tristan@gmail.com>
2016-03-10 14:56:52 +0100

    Adding overlay to modal

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -24,23 +24,27 @@ var modal = function() {
   var workItem = $('.work-item'),
       modal = $('.modal'),
       closeButton = $('.close'),
+      overlay = $('.overlay'),
       body = $('body'),
       esc = 27;
   // Open modal and disable body-scroll
   workItem.on('click', function(e) {
     e.preventDefault();
     $(this).next(modal).toggleClass('open');
+    overlay.toggleClass('on');
     body.css('overflow', 'hidden');
   });
   // Close modal and enable body-scroll
   closeButton.on('click', function(e) {
     e.preventDefault();
     $(this).parent(modal).removeClass('open');
+    overlay.removeClass('on');
     body.css('overflow', 'scroll');
   });
   $(document).keyup(function(e) {
     if (e.keyCode == esc) {
       modal.removeClass('open');
+      overlay.removeClass('on');
       body.css('overflow', 'scroll');
     }
   });
