c0c9be71a1de009805eb9b78a42bae25fe59eff0
Tristan White <oliver.tristan@gmail.com>
2016-03-13 07:59:11 +0100

    reorganising javascript

--- _site/assets/js/functions.js
+++ _site/assets/js/functions.js
@@ -1,20 +1,23 @@
 /******* Responsive Navigation *******/
 var responsiveNav = function() {
-  $('.toggle-menu').on('click', function() {
-     $('#navigation').toggleClass('nav-active');
+  var toggleMenu = $('.toggle-menu'),
+      nav = $('#navigation'),
+      navLink = $('#navigation nav a');
+  toggleMenu.on('click', function() {
+     nav.toggleClass('nav-active');
      $(this).toggleClass('cross');
      $(this).toggleClass('toggle-menu-active');
   });
-  $('#navigation nav a').on('click', function() {
-     $('#navigation').toggleClass('nav-active');
-     $('.toggle-menu').removeClass('toggle-menu-active');
+  navLink.on('click', function() {
+     nav.toggleClass('nav-active');
+     toggleMenu.removeClass('toggle-menu-active');
   });
   $(window).resize(function(){
      var wWidth = $(this).width();
      if( wWidth > 992) {
-        $('#navigation').removeClass('nav-active');
-        $('.toggle-menu').removeClass('cross');
-        $('.toggle-menu').removeClass('toggle-menu-active');
+        nav.removeClass('nav-active');
+        toggleMenu.removeClass('cross');
+        toggleMenu.removeClass('toggle-menu-active');
      }
   });
 };
